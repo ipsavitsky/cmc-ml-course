1dbf53f55f8d269a0834286b6d85c7958380ff18
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-12-26 22:45:28 +0300

    added linear regression task

---
+++ 3.Linear-models/Task.py
@@ -0,0 +1,120 @@
+import numpy as np
+
+
+class Preprocesser:
+    
+    def __init__(self):
+        pass
+    
+    def fit(self, X, Y=None):
+        pass
+    
+    def transform(self, X):
+        pass
+    
+    def fit_transform(self, X, Y=None):
+        pass
+    
+    
+class MyOneHotEncoder(Preprocesser):
+    
+    def __init__(self, dtype=np.float64):
+        super(Preprocesser).__init__()
+        self.dtype = dtype
+        
+    def fit(self, X, Y=None):
+        """
+        param X: training objects, pandas-dataframe, shape [n_objects, n_features]
+        param Y: unused
+        """
+        #your code here
+    
+    def transform(self, X):
+        """
+        param X: objects to transform, pandas-dataframe, shape [n_objects, n_features]
+        returns: transformed objects, numpy-array, shape [n_objects, |f1| + |f2| + ...]
+        """
+        #your code here
+    
+    def fit_transform(self, X, Y=None):
+        self.fit(X)
+        return self.transform(X)
+    
+    def get_params(self, deep=True):
+        return {"dtype": self.dtype}
+    
+    
+class SimpleCounterEncoder:
+    
+    def __init__(self, dtype=np.float64):
+        self.dtype=dtype
+        
+    def fit(self, X, Y):
+        """
+        param X: training objects, pandas-dataframe, shape [n_objects, n_features]
+        param Y: target for training objects, pandas-series, shape [n_objects,]
+        """
+        #your code here
+            
+    def transform(self, X, a=1e-5, b=1e-5):
+        """
+        param X: objects to transform, pandas-dataframe, shape [n_objects, n_features]
+        param a: constant for counters, float
+        param b: constant for counters, float
+        returns: transformed objects, numpy-array, shape [n_objects, 3]
+        """
+        #your code here
+    
+    def fit_transform(self, X, Y, a=1e-5, b=1e-5):
+        self.fit(X, Y)
+        return self.transform(X, a, b)
+    
+    def get_params(self, deep=True):
+        return {"dtype": self.dtype}
+
+    
+def group_k_fold(size, n_splits=3, seed=1):
+    idx = np.arange(size)
+    np.random.seed(seed)
+    idx = np.random.permutation(idx)
+    n_ = size // n_splits
+    for i in range(n_splits - 1):
+        yield idx[i * n_ : (i + 1) * n_], np.hstack((idx[:i * n_], idx[(i + 1) * n_:]))
+    yield idx[(n_splits - 1) * n_ :], idx[:(n_splits - 1) * n_]
+
+    
+class FoldCounters:
+    
+    def __init__(self, n_folds=3, dtype=np.float64):
+        self.dtype = dtype
+        self.n_folds = n_folds
+        
+    def fit(self, X, Y, seed=1):
+        """
+        param X: training objects, pandas-dataframe, shape [n_objects, n_features]
+        param Y: target for training objects, pandas-series, shape [n_objects,]
+        param seed: random seed, int
+        """
+        #your code here
+            
+    def transform(self, X, a=1e-5, b=1e-5):
+        """
+        param X: objects to transform, pandas-dataframe, shape [n_objects, n_features]
+        param a: constant for counters, float
+        param b: constant for counters, float
+        returns: transformed objects, numpy-array, shape [n_objects, 3]
+        """
+        #your code here
+        
+    def fit_transform(self, X, Y, a=1e-5, b=1e-5):
+        self.fit(X, Y)
+        return self.transform(X, a, b)
+ 
+       
+def weights(x, y):
+    """
+    param x: training set of one feature, numpy-array, shape [n_objects,]
+    param y: target for training objects, numpy-array, shape [n_objects,]
+    returns: optimal weights, numpy-array, shape [|x unique values|,]
+    """
+    #your code here
